f14a7f193c9665bf64410b4f78ad1bf45f493fb8
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-31 05:30:11 +0700

    List_id

---
+++ booking-car-that/src/app/admin/list/[...id]/page.tsx
@@ -0,0 +1,148 @@
+import Sidebar from "@/app/_components/Admin/Sidebar/Sidebar";
+import Image from "next/image";
+import React from "react";
+
+type Props = any;
+
+export default function List_id({
+  params,
+}: Props): React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> {
+  const { id } = params;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <Sidebar />
+      <div className="w-[82%] h-full overflow-y-scroll text-gray-500 p-10">
+        <div className=" h-[20rem] w-full overflow-hidden flex justify-around items-center">
+          <div className="w-fit h-fit rounded-lg overflow-hidden">
+            <Image src={"/profile.jpg"} width={300} height={500} alt="" />
+          </div>
+          <div className="w-1/2 h-full rounded-lg">
+            <h1 className="h-16 text-black flex justify-center items-center text-3xl">
+              ข้อมูลผู้จอง
+            </h1>
+            <div className="w-full h-[80%]">
+              <div className="relative overflow-x-auto h-full">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        รหัสผู้จอง
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        ชื่อ-นามสกุล
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        เบอร์ติดต่อ
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        สิทธิ์
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="bg-white  dark:bg-gray-800 dark:border-gray-700">
+                      <td className="px-6 py-4 text-black text-xs">1</td>
+                      <td className="px-6 py-4 text-black text-xs">
+                        ธวิชชัย บุญส่ง
+                      </td>
+                      <td className="px-6 py-4 text-black text-xs">
+                        0652974104
+                      </td>
+                      <td className="px-6 py-4 text-black text-xs">User</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-[20rem] w-full mt-10 flex">
+          <h1 className="w-full h-full text-black flex justify-center items-center text-3xl">
+            ใบขับขี่
+          </h1>
+          <div className="w-full h-full overflow-hidden flex justify-center items-center">
+            <Image
+              src={"/profile.jpg"}
+              height={300}
+              width={300}
+              alt=""
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+        <div className=" h-[20rem] w-full overflow-hidden flex justify-around items-center mt-10 ">
+          <div className="w-fit h-fit rounded-lg overflow-hidden">
+            <Image src={"/profile.jpg"} width={300} height={500} alt="" />
+          </div>
+          <div className="w-[50%] h-full rounded-lg">
+            <h1 className="h-16 text-black flex justify-center items-center text-3xl">
+              ข้อมูลรถ
+            </h1>
+            <div className="w-full h-[80%]">
+              <div className="relative overflow-x-scroll h-full">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        รหัสรถ
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        ยี่ห้อ
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        รุ่น
+                      </th>
+
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        สี
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        ป้ายทะเบียน
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        ที่นั่ง
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        ราคา
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-gray-400">
+                        ประเภทรถ
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="bg-white  dark:bg-gray-800 dark:border-gray-700">
+                      <td className="px-6 py-4 text-black text-xs">1</td>
+                      <td className="px-6 py-4 text-black text-xs">Nissan</td>
+                      <td className="px-6 py-4 text-black text-xs">GTR-35</td>
+                      <td className="px-6 py-4 text-black text-xs">#fff</td>
+                      <td className="px-6 py-4 text-black text-xs">
+                        15 KA Liver
+                      </td>
+                      <td className="px-6 py-4 text-black text-xs">6</td>
+                      <td className="px-6 py-4 text-black text-xs">1500</td>
+                      <td className="px-6 py-4 text-black text-xs">Sport</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-[10rem] w-full mt-10 flex justify-center items-center">
+          <div className="w-1/2 h-1/2 p-4 flex justify-around items-center">
+            <button className="w-1/4 h-[2rem] text-white bg-green-500 rounded-lg active:scale-90 transition-all ease-in-out" onClick={his}>
+              กลับ
+            </button>
+            <button className="w-1/4 h-[2rem] text-white bg-red-500 rounded-lg active:scale-90 transition-all ease-in-out">
+              ลบ
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- booking-car-that/src/app/admin/list/page.tsx
+++ booking-car-that/src/app/admin/list/page.tsx
@@ -1,11 +1,100 @@
-import React from "react";
+"use client";
+import Sidebar from "@/app/_components/Admin/Sidebar/Sidebar";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
-export default function page({}: Props) {
+export default function List({}: Props) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getCars();
+  }, []);
+  const getCars = async () => {
+    await fetch("/api/booking")
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  };
+
   return (
-    <div className="text-black text-8xl flex justify-center items-center text-center font-semibold w-full h-full">
-      ถ้าเศร้าให้กินเหล้าสิบเป๊ก <br />ถ้าอยากตรงสเป๊ก ให้กินสเป๊กสิบเหล้า
+    <div className="w-full h-full flex justify-center items-center">
+      <Sidebar />
+      <div className="h-full w-[82%] p-2">
+        <div className="h-[10%] w-full p-2 flex justify-end">
+          <div className="w-[20%] h-full flex justify-center items-center text-blue-500">
+            จำนวนรายการจอง : {Object.keys(data).length}
+          </div>
+        </div>
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr className="text-black">
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  รหัสการจอง
+                </th>
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  ยี่ห้อรถ
+                </th>
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  รุ่นรถ
+                </th>
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  ชื่อผู้จอง
+                </th>
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  วันที่รับรถ
+                </th>
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  วันที่คืนรถ
+                </th>
+                <th scope="col" className="px-6 py-3 text-black font-normal">
+                  ราคา (฿)
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, index) => {
+                return (
+                  <tr
+                    key={index}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <td className="px-6 py-4 text-black text-xs">
+                      {item["bid"]}
+                    </td>
+                    <td className="px-6 py-4 text-black text-xs">
+                      {item["cbrand"]}
+                    </td>
+                    <td className="px-6 py-4 text-black text-xs">
+                      {item["cmodel"]}
+                    </td>
+                    <td className="px-6 py-4 text-black text-xs">
+                      {item["uname"]}
+                    </td>
+                    <td className="px-6 py-4 text-black text-xs">
+                      {item["bdate_s"]}
+                    </td>
+                    <td className="px-6 py-4 text-black text-xs">
+                      {item["bdate_e"]}
+                    </td>
+                    <td className="px-6 py-4 text-green-500 text-xs">
+                      {item["cprice"]} ฿
+                    </td>
+                    <td className="px-6 py-4 text-black text-xs action">
+                      <button className="mr-10 text-yellow-500">
+                        <Link href={`/admin/list/${item['bid']}`} className="h-full w-full">
+                          ดูรายละเอียด
+                        </Link>
+                      </button>
+                      <button className="text-red-500">ลบ</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
