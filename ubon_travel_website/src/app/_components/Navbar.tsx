2856b582db1b72140a961b4bf3fd9bed6bd8cfaf
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-09-03 00:15:45 +0700

    Update Navbar.tsx

--- ubon_travel_website/src/app/_components/Navbar.tsx
+++ ubon_travel_website/src/app/_components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -7,30 +7,28 @@ import React from "react";
 type Props = {};
 
 export default function Navbar({}: Props) {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="w-full h-fit">
       <div className="border h-[4rem] w-full bg-rose-500 flex justify-between items-center pl-5 pr-5">
         <div className="w-[20rem]">
-          <Image src={"/lotus.png"} width={50} height={50} alt="Logo" onClick={()=>{
-            return router.replace('/')
-          }}/>
+          <Image
+            src={"/lotus.png"}
+            width={50}
+            height={50}
+            alt="Logo"
+            onClick={() => {
+              return router.replace("/");
+            }}
+          />
         </div>
         <h1 className="text-white text-3xl">TEAVEL UBON RATCHATHANI</h1>
-        <div className="w-[20rem] h-full flex justify-between items-center">
+        <div className="w-[20rem] h-full flex justify-end items-center">
           <Link
             href={"/authen/login"}
-            className="ring-1 ring-white w-[48%] h-[60%] active:scale-90 transition-all ease-in-out"
+            className=" w-[48%] h-[60%] active:scale-90 transition-all ease-in-out bg-white rounded-lg"
           >
-            <button className="w-full h-full text-white ">ล็อคอิน</button>
-          </Link>
-          <Link
-            href={""}
-            className="ring-1 ring-white w-[48%] h-[60%] active:scale-90 transition-all ease-in-out"
-          >
-            <button className="w-full h-full bg-white text-rose-500 ">
-              ลงทะเบียน
-            </button>
+            <button className="w-full h-full text-lg text-red-500 ">ล็อคอิน</button>
           </Link>
         </div>
       </div>
