6170db898885327aa6a62e41908b45b4c1f8b171
PhongsathonNamsaen <std.64122420215@ubru.ac.th>
2024-10-16 23:45:22 -0700

    asd

--- ubon_travel_website/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      // If you want to add external image domains later, you can add remotePatterns here
-    },
-  };
-  
-  export default nextConfig;
-  
